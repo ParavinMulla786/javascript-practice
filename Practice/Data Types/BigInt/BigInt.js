281e12e162f4011421eb68cf57894c593dc92fd4
paravinmullananotechsoftwares <paravinmulla2004@gmail.com>
2026-03-24 20:41:31 +0530

    String

--- Practice/Data Types/BigInt/BigInt.js
+++
@@ -1,8 +0,0 @@
-// Create a BigInt
-let bigNumber = 123456789123456789n;
-
-// Multiply by 2
-let result = bigNumber * 2n;
-
-// Output
-console.log("Result:", result);
---
+++ Practice/String/index.js
@@ -0,0 +1,11 @@
+let pone = "Success is the sum of small efforts, repeated day in and day out";
+let ptwo = pone.replaceAll(" ", "_");
+
+console.log(ptwo);
+
+
+// 5
+
+let fruit = "PineApple, Mango , Orange";
+let fruitsCollection = fruit.split();
+console.log(fruitsCollection);
